f0297f83b6be044fc6b6423cc7d66bbcd0521435
la2697 <56022392+la2697@users.noreply.github.com>
2020-11-22 14:59:14 +0100

    Änderung

--- GUI Oberfläche Zahnräder/GUI Oberfläche Zahnräder/MainWindow.xaml.cs
+++ GUI Oberfläche Zahnräder/GUI Oberfläche Zahnräder/MainWindow.xaml.cs
@@ -18,12 +18,49 @@ namespace GUI_Oberfläche_Zahnräder
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window 
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+            rtb_S235JR.Visibility = Visibility.Hidden;
+            rtb_EN_GJS.Visibility = Visibility.Hidden;
+            rtb_EN_GJL.Visibility = Visibility.Hidden;
+            lb_Werkstoffwahl.Visibility = Visibility.Hidden;
+
+            rtb_Standard.Visibility = Visibility.Hidden;
+            rtb_0_1.Visibility = Visibility.Hidden;
+            rtb_0_3.Visibility = Visibility.Hidden;
+            lb_Kopfspiel.Visibility = Visibility.Hidden;
+
+            Im_GeradZahnrad.Visibility = Visibility.Hidden;
+            Im_Innenzahnrad.Visibility = Visibility.Hidden;
+            Im_Kegelrad.Visibility = Visibility.Hidden;
+            Im_SchrägZahnrad.Visibility = Visibility.Hidden;
+
+            txt_Breite.Visibility = Visibility.Hidden;
+            txt_Modul.Visibility = Visibility.Hidden;
+            txt_Teilkreis.Visibility = Visibility.Hidden;
+            txt_Zähne.Visibility = Visibility.Hidden;
+
+            lb_Breite.Visibility = Visibility.Hidden;
+            lb_Modul.Visibility = Visibility.Hidden;
+            lb_Teilkreisdurchmesser.Visibility = Visibility.Hidden;
+            lb_Zähnezahl.Visibility = Visibility.Hidden;
+
+            
+            
         }
 
+      
+
+       
+
+        
     }
+   
+
+
+
 }
